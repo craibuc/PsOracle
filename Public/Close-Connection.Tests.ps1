f56952cf38485c62aca65b19d832e0b0526fbd15
Craig Buchanan <craig.buchanan@cogniza.com>
2017-01-13 05:46:46 -0800

    initial commit

---
+++ Public/Close-Connection.Tests.ps1
@@ -0,0 +1,34 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe "Close-Connection" {
+
+    BeforeEach {
+
+        # load credentials from PSD1 file
+        Import-LocalizedData -FileName 'com.oracle.credentials.psd1' -BindingVariable Credentials
+        Write-Debug $Credentials.ServerInstance
+        Write-Debug $Credentials.Username
+        Write-Debug $Credentials.SecurePassword
+
+        $params = @{
+            ServerInstance=$Credentials.ServerInstance;
+            Username=$Credentials.Username;
+            # convert secure text to a SecureString object
+            Password = $Credentials.SecurePassword | ConvertTo-SecureString
+        }
+
+        Open-Connection @params
+
+    }
+
+    It "Closes an connection" {
+        # act
+        $actual = Close-Connection -Verbose
+        
+        # assert
+        $global:connection | Should BeNullOrEmpty
+    }
+
+}
---
+++ Public/Close-Connection.ps1
@@ -0,0 +1,12 @@
+﻿function Close-Connection {
+
+    [CmdletBinding()]
+    param()
+
+    if ( $global:Connection ) {
+        $global:Connection.Close()
+        $global:Connection.Dispose()
+        $global:Connection = $null
+    }
+
+}
